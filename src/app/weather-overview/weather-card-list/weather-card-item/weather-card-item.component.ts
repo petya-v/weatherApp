cc3929df474dafa76cbfd2afc11db0ef838591fc
petya <petya.voynishka@gmail.cm>
2018-11-15 18:14:14 +0200

    Add weather card item

---
+++ src/app/weather-overview/weather-card-list/weather-card-item/weather-card-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-card-item',
+  templateUrl: './weather-card-item.component.html',
+  styleUrls: ['./weather-card-item.component.scss']
+})
+export class WeatherCardItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
