fde6d311affdb035f82f6b3b87d6a235f851d77a
petya <petya.voynishka@gmail.cm>
2018-11-18 21:52:38 +0200

    Add animations

--- src/app/weather-overview/weather-card-list/weather-card-item/weather-card-image/weather-card-image.component.ts
+++ src/app/weather-overview/weather-card-list/weather-card-item/weather-card-image/weather-card-image.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Weather} from '../../../../models/weather-item';
 
 @Component({
   selector: 'app-weather-card-image',
@@ -6,10 +7,13 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./weather-card-image.component.scss']
 })
 export class WeatherCardImageComponent implements OnInit {
+  @Input() weatherAnimation: Weather;
+  private weather: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.weather = this.weatherAnimation[0].main;
   }
 
 }
